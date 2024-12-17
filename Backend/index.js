c6c5a78861ec0bfa46e3ef7aa3975b6e5c847ba7
info-aaditya <gaadityag@gmail.com>
2024-12-17 13:49:03 +0545

    Impl: Mongodb Connection

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,14 @@
-const express = require('express');
+import express from 'express';
+import dotenv from 'dotenv';
+
+import { connectDB } from './src/config/db.js';
+
+dotenv.config();
+
 const app = express();
 
 // Set the server port
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
@@ -12,7 +18,8 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Trip Planner Server!');
 });
 
-// Start the server
+// Start the server and establish database connection
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
 });
